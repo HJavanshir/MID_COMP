767593868ea3ad995b255449176ecf1b87db066a
Hamoun Javanshir <hamoun.javanshir@gmail.com>
2021-11-08 20:22:26 -0500

    commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Hamoun:1q2w3e4r@cluster0.knrib.mongodb.net/books229?retryWrites=true&w=majority",
+};
